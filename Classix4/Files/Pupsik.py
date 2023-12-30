1781c1d9da44777b7850f2518f7abac1e73283d2
mamype <mamype@duck.com>
2023-12-31 05:12:25 +1000

    ok?

--- Classix4/Files/Pupsik.py
+++
@@ -1,6 +0,0 @@
-resx = 800
-resy = 600
-color = "violet"
-bg = "pink"
-bgtext = "Я ПУПСИК XDXDXDXDXD"
-fontsize = 10
--- Classix4/Files/yaroslaw.py
+++
@@ -1,6 +0,0 @@
-resx = 1280
-resy = 720
-color = "red"
-bg = "black"
-bgtext = "привет пупсик"
-fontsize = 10
